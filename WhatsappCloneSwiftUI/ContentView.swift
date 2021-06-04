ac0b8d4666841d703c80f1eb13ff2ba15c3e6e21
Bilgehan KAYA <bilgehankaya79@gmail.com>
2021-06-04 19:18:46 +0300

    Initial Commit

---
+++ WhatsappCloneSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhatsappCloneSwiftUI
+//
+//  Created by Bilgehan KAYA on 4.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
